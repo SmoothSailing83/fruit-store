5dad4d42f559dffd575e7fcde9b72f71a882b744
yifguo <yifanguo83@gmail.com>
2021-06-19 01:12:00 -0400

    first commit

---
+++ src/app/fruits.service.ts
@@ -0,0 +1,15 @@
+   // src/app/fruits.service.ts
+    
+   import { Injectable } from '@angular/core';
+   import { HttpClient } from '@angular/common/http';
+   
+   @Injectable({
+     providedIn: 'root'
+   })
+   export class FruitsService {
+     constructor(private http: HttpClient) {}
+   
+     getAll() {
+       return this.http.get('http://localhost:4000/fruits');
+     }
+   }
